065d16b35c2157cc8be13293ae0e6ff364ac2933
Amogh Vishwakarma <amoghvk@icloud.com>
2022-12-15 15:14:44 +0000

    test website working, table created

--- hospital/models.py
+++ hospital/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    role = models.CharField(max_length=1, blank=False, null=False, default="p")
+    treatedBy = models.ForeignKey('self', on_delete=models.DO_NOTHING, blank=True, null=True)
+    history = models.CharField(max_length=1000, blank=True, null=True)
+    img = models.ImageField(blank=True, null=True)
+    diagnosis = models.CharField(max_length=20, blank=True, null=True)
--- hospital/views.py
+++ hospital/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponse
 
-# Create your views here.
+
+def test_view(request):
+
+    
+    return render(request, "hospital/index.html")
